321a92739329d79b5576cdc1471f2f76b0c75aef
João Marcus de Lemos Fernandes <jmldesignergrafico@gmail.com>
2016-10-02 14:40:27 -0300

    Added css autoprefixer

--- spec/views/landing_pages/index.html.erb_spec.rb
+++ spec/views/landing_pages/index.html.erb_spec.rb
@@ -2,10 +2,20 @@ require 'rails_helper'
 
 RSpec.describe "landing_pages/index.html.erb", type: :view do
   describe 'Entering the Index page' do
-    it 'should display default generated text' do
-      visit landing_pages_index_path
 
-      expect(page).to have_text 'LandingPages#index'
+    before :each do
+      visit landing_pages_index_path
+    end
+
+    it 'should display default generated text' do
+
+      expect(page).to have_text 'Hello, folks!'
+    end
+
+    it 'should go back to index page' do
+      click_link 'Home'
+
+      expect(page).to have_text 'Hello, folks!'
     end
   end
 end
