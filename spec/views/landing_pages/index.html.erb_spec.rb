7752b3c150bab4099d4547982ff824234a56f927
João Marcus de Lemos Fernandes <jmldesignergrafico@gmail.com>
2016-10-02 18:03:07 -0300

    Ended Home page

--- spec/views/landing_pages/index.html.erb_spec.rb
+++ spec/views/landing_pages/index.html.erb_spec.rb
@@ -9,13 +9,13 @@ RSpec.describe "landing_pages/index.html.erb", type: :view do
 
     it 'should display and welcome text when entering the page' do
 
-      expect(page).to have_text 'Hello, folks!'
+      expect(page).to have_text 'Ultra Coffee Lover'
     end
 
     it 'should go back to index page when main link is clicked' do
       click_link '@joaomarcuslf'
 
-      expect(page).to have_text 'Hello, folks!'
+      expect(page).to have_text 'Ultra Coffee Lover'
     end
   end
 end
